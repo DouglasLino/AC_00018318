d9415c1d6a16cd19ee41b10373eef8d54af5c032
DouglasLino <00018318@uca.edu.sv>
2021-05-10 23:56:00 -0600

    labo3: ejercicio 1, disvision

--- Tarea Laboratorio 3/ejercicio1.asm
+++ Tarea Laboratorio 3/ejercicio1.asm
@@ -5,7 +5,7 @@ section	.text
         XOR SI, SI
         XOR AX, AX
         XOR DI, DI
-        MOV byte CX, 5d         ; tamaño de la variable
+        MOV byte CX, 5d         ; tamaño de la variable (carnet)
         JMP iterar              ;salto sin condicion
 iterar:
         MOV AL, [msj+DI]         ;en la primera iteracion DI =0, se agarra la primera letra
@@ -14,10 +14,15 @@ iterar:
         INC DI                  ; Se incrementa DI -> DI++
         LOOP iterar 
 exit:
-        mov BYTE[20Ah], BL      ; lo muevo a la direccion que quieren
-    	int	20h
+        MOV AL,BL               ;muevo el total(BL) a AL(porque aqui va el diviendo)
+        XOR BX, BX              ; limpio BX (por comodidad)
+        MOV BL, 8h              ;muevo 8 a BL(el divisor)
+        DIV BL                  ; lo divido
+        mov BYTE[20Ah], AL      ; lo muevo a la direccion que quieren (AL porque alli quedo el cociento de 8 bits)
+    	int	20h             
 
 section .data
-msj DB  1d,8d,3d,1d,8d              ;mi correo es 00018318, le quite los ceros
+msj DB  1d,8d,3d,1d,8d           ;mi carnet es 00018318, le quite los ceros
 
                                 ; la suma = 21 tiene que dar 15 en hexa
+                                ; mi promedio seria 21/8 = 2.625
